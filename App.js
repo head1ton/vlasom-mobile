765101ef36c8b83dbbe710e83bbd780bca78acb0
SEUNGMIN KIM <smk950226@naver.com>
2018-10-21 02:27:51 +0900

    preload assets

--- App.js
+++ App.js
@@ -1,13 +1,70 @@
 import React from 'react';
+import { AppLoading, Asset, Font } from 'expo';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  }
   render() {
+    const { isLoadingComplete } = this.state;
+    if(!isLoadingComplete){
+      return (
+        <AppLoading  
+        startAsync={this._loadAssetsAsync} 
+        onError={this._handleLoadingError} 
+        onFinish={this._handleFinishLoading} 
+        />
+      )
+    }
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
+  }
+
+  _loadAssetsAsync = async() => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/female-before.png'),
+        require('./assets/images/female.png'),
+        require('./assets/images/history-red.png'),
+        require('./assets/images/history.png'),
+        require('./assets/images/home-red.png'),
+        require('./assets/images/home.png'),
+        require('./assets/images/like-list-red.png'),
+        require('./assets/images/like-list.png'),
+        require('./assets/images/login-pw.png'),
+        require('./assets/images/logo-large-white.png'),
+        require('./assets/images/logo-small-red.png'),
+        require('./assets/images/logo-small-white.png'),
+        require('./assets/images/logo-xlarge-red.png'),
+        require('./assets/images/male-before.png'),
+        require('./assets/images/male.png'),
+        require('./assets/images/menu-white.png'),
+        require('./assets/images/more-red.png'),
+        require('./assets/images/more.png'),
+        require('./assets/images/nav-nickname.png'),
+        require('./assets/images/profile-red.png'),
+        require('./assets/images/search.png'),
+      ]),
+      Font.loadAsync({
+        ...Ionicons.font,
+        ...MaterialIcons.font
+      })
+    ])
+  }
+
+  _handleLoadingError = error => {
+    console.error(error);
+  }
+
+  _handleFinishLoading = async() => {
+    this.setState({
+      isLoadingComplete: true
+    })
   }
 }
 
@@ -19,3 +76,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+export default App;
