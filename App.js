2126f8c2c8659ce2ddd46bb9648c27ff207b0044
SEUNGMIN KIM <smk950226@naver.com>
2018-10-21 03:32:59 +0900

    basic app container

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import configureStore from './redux/configureStore';
+import AppContainer from './components/AppContainer';
 
 const { persistor, store } = configureStore();
 
@@ -26,9 +27,7 @@ class App extends React.Component {
     return (
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-          </View>
+          <AppContainer />
         </PersistGate>
       </Provider>
     );
@@ -76,15 +75,5 @@ class App extends React.Component {
     })
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 
 export default App;
