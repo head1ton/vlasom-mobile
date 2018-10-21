86cc03a0fb12d5bb918a79c9547e895bdc06254f
SEUNGMIN KIM <smk950226@naver.com>
2018-10-21 21:09:00 +0900

    nav btn

--- routes/sharedRoutes.js
+++ routes/sharedRoutes.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import LikeScreen from '../screens/LikeScreen';
 import CommentScreen from '../screens/CommentScreen';
+import NavButton from '../components/NavButton';
 
 const sharedRoutes = {
     Like: {
@@ -8,15 +10,16 @@ const sharedRoutes = {
     Comment: {
         screen: CommentScreen
     }
-}
+};
 
 const sharedOptions = {
     navigationOptions: {
+        headerLeft: props => <NavButton {...props} iconName={"ios-arrow-back"} />,
         headerStyle: {
             backgroundColor: '#ffffff'
         }
     }
-}
+};
 
 export { sharedOptions };
 
