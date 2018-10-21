b64cd42430a11870059329424267673d8f33abf8
SEUNGMIN KIM <smk950226@naver.com>
2018-10-21 23:26:09 +0900

    scroll view for feed

--- screens/FeedScreen/container.js
+++ screens/FeedScreen/container.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import FeedScreen from './presenter';
 
 class Container extends Component{
+    state = {
+        isFetching: false
+    }
+
+    static propTypes = {
+        feed: PropTypes.array,
+        getFeed: PropTypes.func.isRequired
+    }
+
+    componentWillReceiveProps = (nextProps) => {
+        if(nextProps.feed){
+            this.setState({
+                isFetching: false
+            })
+        }
+    }
+
     render(){
         return (
-            <FeedScreen {...this.props} />
+
+            <FeedScreen {...this.props} {...this.state} refresh={this._refresh} />
         )
+    }
+
+    _refresh = () => {
+        const { getFeed } = this.props;
+        this.setState({
+            isFetching: true
+        })
+        getFeed();
     }
 };
 
